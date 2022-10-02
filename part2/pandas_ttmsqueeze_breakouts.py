0c1b6373a91624feec8b42be52e5940077c3d544
Romano Vacca <romanovacca@Romanos-MacBook-Pro.local>
2022-10-02 21:02:22 +0200

    changed logger names and place where i measure time

--- part2/pandas_ttmsqueeze_breakouts.py
+++ part2/pandas_ttmsqueeze_breakouts.py
@@ -14,7 +14,6 @@ def run_indicator_for_all_files(
 ):
     dataloader = DataLoader()
 
-    start_time = time.time()
     for file in os.listdir(path):
         if file.endswith(".csv"):
             breakout = squeeze_indicator.run(
@@ -22,7 +21,6 @@ def run_indicator_for_all_files(
             )
             if breakout:
                 logger.info(breakout)
-    logger.info("whole loop --- %s seconds ---" % (time.time() - start_time))
 
     return
 
@@ -31,11 +29,13 @@ def main():
     logger.info("Started main.")
 
     ttm_squeeze = TTMSqueeze()
+    start_time = time.time()
     run_indicator_for_all_files(
         path="data/USDT/",
         intervals=["1h", "2h", "4h", "8h", "12h", "1d", "2d", "3d", "7d"],
         squeeze_indicator=ttm_squeeze,
     )
+    logger.info("whole loop --- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
--- part2/polars_ttmsqueeze_breakouts.py
+++ part2/polars_ttmsqueeze_breakouts.py
@@ -14,7 +14,6 @@ def run_indicator_for_all_files(
 ):
     dataloader = DataLoader()
 
-    start_time = time.time()
     for file in os.listdir(path):
         if file.endswith(".csv"):
             breakout = squeeze_indicator.run(
@@ -22,21 +21,22 @@ def run_indicator_for_all_files(
             )
             if breakout:
                 logger.info(breakout)
-    logger.info("whole loop --- %s seconds ---" % (time.time() - start_time))
 
     return
 
 
-def main():
+def main_polars():
     logger.info("Started main.")
 
     ttm_squeeze = TTMSqueeze()
+    start_time = time.time()
     run_indicator_for_all_files(
         path="data/USDT/",
         intervals=["1h", "2h", "4h", "8h", "12h", "1d", "2d", "3d", "1w"],
         squeeze_indicator=ttm_squeeze,
     )
+    logger.info("whole loop --- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
-    main()
+    main_polars()
